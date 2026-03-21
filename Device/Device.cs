e59f0beac82afdda15c9f3c69215208df490e3cc
bobmcw <jasiekz2014@gmail.com>
2026-03-21 14:21:08 +0100

    made IDevice into an abstract class instead of an interface

---
+++ Device/Device.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1.Device;
+
+public abstract class Device
+{
+    public int Id { get; }
+    private static int _idGenerator = 0;
+    protected Device()
+    {
+        this.Id = ++_idGenerator;
+    }
+    
+}
--- Device/IDevice.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ConsoleApp1.Device;
-
-public interface IDevice
-{
-    
-}
