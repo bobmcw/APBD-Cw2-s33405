0d9693a9539cccb08035fca0871717fd9e159f6f
bobmcw <jasiekz2014@gmail.com>
2026-03-22 17:35:32 +0100

    added renting device

--- RentalService/RentalService.cs
+++ RentalService/RentalService.cs
@@ -1,6 +1,25 @@
 namespace ConsoleApp1.RentalService;
 
+using User;
+using Device;
 public class RentalService
 {
-    private Dictionary<Device.Device, RentStatus> _rentDict;
+    private List<Device> _inventory;
+    private List<RentStatus> _currentRents;
+    private List<RentStatus> _history;
+
+    void rentDevice(int id, User user, DateTime returnDate)
+    {
+        var res = _inventory.Where(i => i.Id == id);
+        var items = res.ToList();
+        if (items.Count == 1)
+        {
+            var dev = items.First();
+            _currentRents.Add(new RentStatus(user, dev, returnDate));
+        }
+        else
+        {
+            throw new Exception("element with such ID not found");
+        }
+    }
 }
