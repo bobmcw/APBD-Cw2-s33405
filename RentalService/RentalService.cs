98b59df36adc0d0b530b84406662c5cdef87c161
bobmcw <jasiekz2014@gmail.com>
2026-03-22 18:34:31 +0100

    added getters for fields

--- RentalService/RentalService.cs
+++ RentalService/RentalService.cs
@@ -4,17 +4,21 @@ using User;
 using Device;
 public class RentalService
 {
-    private List<Device> _inventory;
-    private List<RentStatus> _currentRents;
-    private List<RentStatus> _history;
+    public List<Device> Inventory { get; } = new List<Device>();
+    public List<RentStatus> CurrentRents { get; } = new List<RentStatus>();
+    public List<RentStatus> History { get; } = new List<RentStatus>();
 
-    void RentDevice(int id, User user, DateTime returnDate)
+    public void AddDeviceToInventory(Device dev)
     {
-        var items = _inventory.Where(i => i.Id == id).ToList();
+        Inventory.Add(dev);
+    }
+    public void RentDevice(int id, User user, DateTime returnDate)
+    {
+        var items = Inventory.Where(i => i.Id == id).ToList();
         if (items.Count == 1)
         {
             var dev = items.First();
-            _currentRents.Add(new RentStatus(user, dev, returnDate));
+            CurrentRents.Add(new RentStatus(user, dev, returnDate));
         }
         else
         {
@@ -22,14 +26,14 @@ public class RentalService
         }
     }
 
-    void ReturnDevice(int id)
+    public void ReturnDevice(int id)
     {
-        var items = _currentRents.Where(i => i.DeviceId() == id).ToList();
+        var items = CurrentRents.Where(i => i.DeviceId() == id).ToList();
         if (items.Count == 1)
         {
             items[0].returnDevice();
-            _history.Add(items[0]);
-            _currentRents.Remove(items[0]);
+            History.Add(items[0]);
+            CurrentRents.Remove(items[0]);
         }
     }
 }
