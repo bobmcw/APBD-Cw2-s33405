0b76b8417af935357b9be1a4400faad4e2734f43
bobmcw <jasiekz2014@gmail.com>
2026-03-22 17:35:21 +0100

    added device to RentStatus

--- RentalService/RentStatus.cs
+++ RentalService/RentStatus.cs
@@ -13,12 +13,14 @@ public class RentStatus
     private DateTime _declaredReturnDate;
     private DateTime _returnDate;
     private User.User? _user;
+    private Device.Device? _device;
     private State _state = State.Available;
 
-    void rentDevice(User.User user, DateTime returnDate)
+    public RentStatus(User.User user, Device.Device device,DateTime returnDate)
     {
         if (_state == State.Available)
         {
+            _device = device;
             _state = State.Rented;
             _start = DateTime.Now;
             _user = user;
@@ -30,7 +32,7 @@ public class RentStatus
         }
     }
     
-    void returnDevice()
+    public void returnDevice()
     {
         _state = State.Returned;
         _returnDate = DateTime.Now;
